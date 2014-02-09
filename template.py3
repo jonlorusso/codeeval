11f7be83ad4bbc780f7cf3d2fef6a362adaf2437
Jon Lorusso <jonlorusso@gmail.com>
2014-02-08 20:26:59 -0500

    Create templates dir.

--- template.py3
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python
-
-import sys
-
-for line in open(sys.argv[1], 'r'):
-    print(line)
---
+++ templates/template.js
@@ -0,0 +1,15 @@
+#!/usr/bin/env node
+
+var fs  = require("fs");
+
+fs.readFileSync(process.argv[2]).toString().split('\n').forEach(function (line) {
+    if (line != "") {       
+        line = line.replace(/\r/gm, ""); // For Windows.
+
+        var arr = line.split(" "); // Splits the string by space
+
+        var diff = arr[0] - arr[1];
+
+        console.log(diff);        
+    }
+})
